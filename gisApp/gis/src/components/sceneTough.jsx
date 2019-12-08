818193f767b78ae8e9b5c3e9892120df44073f9e
lilyht <295808612@qq.com>
2019-12-08 14:08:36 +0800

    更改景点信息栏

--- gisApp/gis/src/components/sceneTough.jsx
+++ gisApp/gis/src/components/sceneTough.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { fetchData } from '../utils/request'
+import "../style/components/sceneTough.css"
+
 let list = ''
 let datarray = []
 
--- gisApp/gis/src/containers/scene/scene.jsx
+++ gisApp/gis/src/containers/scene/scene.jsx
@@ -4,6 +4,7 @@ import SceneDetail from '../../components/sceneDetail'
 import { fetchData } from '../../utils/request'
 import SceneTough from '../../components/sceneTough'
 import "../../style/containers/scene.css"
+import { Icon } from 'antd';
 
 let datarray = []
 let sceneID
@@ -93,7 +94,7 @@ class Scene extends React.Component {
         return (
             <div id='scene'>
                 <div className='back' onClick={this.handleBack}>
-                    <svg t="1575646291983" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4438" width="48" height="48"><path d="M766.862 1021.724c-10.24 0-20.594-3.64-28.444-10.922L228.693 538.396c-15.701-14.563-15.701-38.229 0-52.792L738.418 13.198c15.701-14.563 41.187-14.563 56.889 0 15.701 14.564 15.701 38.23 0 52.793L314.027 512l481.28 446.009c15.701 14.563 15.701 38.23 0 52.793-7.851 7.282-18.205 10.922-28.445 10.922z" fill="#333333" p-id="4439"></path></svg>
+                    <Icon type="left" />返回
                 </div>
                 {this.state.detail ? <SceneDetail datarray={datarray} /> : null}
                 <SceneMap sceneID={sceneID} />
