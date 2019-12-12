53c3054a7a45890318cf63a1b55e4387f746b087
lilyht <295808612@qq.com>
2019-12-12 23:43:31 +0800

    右

--- gisApp/gis/src/components/sceneTough.jsx
+++ gisApp/gis/src/components/sceneTough.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Icon } from 'antd';
 import PropTypes from 'prop-types';
 import { fetchData } from '../utils/request'
 import "../style/components/sceneTough.css"
@@ -26,10 +27,16 @@ class SceneTough extends React.Component {
                     </div>
                     <div className='main'>
                         <div className='type_score'>
-                            <div className='type'>{datarray.type||datarray.hotel_distance||datarray.traffic_distance||datarray.res_type}</div>
-                            <div className='score'>{datarray.score||datarray.hotel_score||datarray.res_price}</div>
+                            <div className='type'><Icon type="home" style={{ color: '#1890ff'}}/><span></span>
+                                {datarray.type||datarray.hotel_distance||datarray.traffic_distance||datarray.res_type}</div>
+                            <div className='score'><Icon type="message" style={{ color: '#1890ff'}}/><span></span>
+                                {datarray.score||datarray.hotel_score}</div>
+                            <div className='resprice'><Icon type="account-book" style={{ color: '#1890ff'}}/><span></span>
+                                人均：{datarray.res_price}
+                            </div>
                         </div>
-                        <div className = 'location'>{datarray.location||datarray.hotel_location||datarray.res_location}</div>
+                        <div className = 'location'><Icon type="environment" style={{ color: '#1890ff'}} /><span></span>
+                            {datarray.location||datarray.hotel_location||datarray.res_location}</div>
                         <div className = 'time'>{datarray.time||datarray.res_distance}</div>
                     </div>
                     <div className = 'phone'>{datarray.telephone}</div>
